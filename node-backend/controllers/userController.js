92c49158b1ba13921e58879fe49e9347862c583f
Raj Kumar <46060.raj@gmail.com>
2022-03-08 23:12:26 +0530

    login basic node

--- node-backend/controllers/userController.js
+++ node-backend/controllers/userController.js
@@ -1,9 +1,24 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports.login = async (req, res) => {
     console.log(req.body);
-    res.send(req.body)
+
+    var user = await User.findOne({where: {email: req.body.email}});
+
+    if(!user)
+        res.status(401).send({message: 'invalid email'})
+    
+    var user = await User.findOne({where: {email: req.body.email}});
+
+    var userVerified = await bcrypt.compare(req.body.password, user.password);
+    
+    if(!userVerified)
+        res.status(401).send({message: 'invalid password'})
+    
+    var token = jwt.sign({email: user.email}, 'secret');
+    res.status(200).send({token})
 }
 
 module.exports.register = async (req, res) => {
